a0840fc1e56e7b3c296bc0a826fc9db0a48865d5
BoxyBoy <ljmathers20@gmail.com>
2020-03-10 20:29:33 -0600

    Added the difficulty class. almost forgot

---
+++ app/src/main/java/com/example/finalprojectminigame/Difficulty.java
@@ -0,0 +1,18 @@
+package com.example.finalprojectminigame;
+
+import android.content.Intent;
+
+public class Difficulty {
+
+    public void difficultyEasy(){
+
+    }
+
+    public void difficultyMedium(){
+
+    }
+
+    public void difficultyHard(){
+
+    }
+}
